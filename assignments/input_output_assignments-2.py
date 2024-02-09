d991726d5030d6ff33a8772dfc15d5591526ee8c
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-02-09 10:39:51 +0530

    Add files via upload

---
+++ assignments/input_output_assignments-2.py
@@ -0,0 +1,17 @@
+"""
+WAP to print the following output, you to write in single of code
+'Hi' : static message using print()
+'Anil' : dynamic using input()
+Final Output : 'Hi Anil'
+"""
+
+"""
+WAP to print the following output in single of code
+'Hello' : dynamic using input()
+'Anil' : dynamic using input()
+Note: please use print() to combine the output
+Final output format:
+Hello Anil
+Hi Anil
+I Love Python
+"""
