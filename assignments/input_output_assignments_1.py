ab90b852583fbd55ec37b54bf289ad3c1af556fc
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-02-12 11:17:26 +0530

    Add files via upload

---
+++ assignments/input_output_assignments_1.py
@@ -0,0 +1,19 @@
+"""
+Assignment-1:
+WAP to print the following output using print function
+1 2 3 4 5
+"""
+
+"""
+Assignment-2:
+# WAP to print the following output using print function
+1 2 3 => Same line, one space between each value
+4    5 => Same line, tab space between each value
+
+"""
+
+"""
+Assignment-3:
+WAP to print 'Hi' on the screen
+and 'I Love Python' to the file using the print function
+"""
