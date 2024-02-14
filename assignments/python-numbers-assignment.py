ede957e38428286d307510bfd381e41c5ac68f6e
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-02-14 21:28:38 +0530

    Add files via upload

---
+++ assignments/python-numbers-assignment.py
@@ -0,0 +1,16 @@
+"""
+WAP to assign 42 and 3.14 values to the variable and constant.
+Check the values and type of those after the assignment.
+"""
+
+'''
+WAP to define one complex number with lower case 'j' and
+another one with the upper case 'J'.
+Check the values and type of the variables after the assignment.
+'''
+
+'''
+WAP to assign 100 digits integer number to a variable.
+Check the value, size and type of the variable after the assignment.
+'''
+
---
+++ assignments/strings-assignment.py
@@ -0,0 +1,13 @@
+"""
+Write a Program to retrieve character 'X' using positive indexing
+from "Semantic segmentation of Lung from Chest X-Ray" string
+"""
+
+'''
+Write a Program to retrieve the 'L' character from "I Love Python Programming" string
+using negative/reverse indexing
+'''
+
+'''
+Can prove that python string is immutable with the program
+'''
