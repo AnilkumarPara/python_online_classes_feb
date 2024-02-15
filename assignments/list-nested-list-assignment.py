fe62d0ba47501dd3af3814bc118fc0bce6c9c813
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-02-15 16:16:55 +0530

    Add files via upload

---
+++ assignments/list-nested-list-assignment.py
@@ -0,0 +1,17 @@
+# WAP to retrieve the value 5 from the following list using negative indexing
+L1 = [21, 9.8, 'hi', 45, 765, 2001, 5, 2034, 'Hey']
+
+# WAP to retrieve the 'Python' string from the following list using Positive indexing
+L2 = [28, 111, 100.5, 'Python', 48, 765, 2201, 5, 2034, 'Java']
+
+# WAP to retrieve the character 'i' from the following list using negative indexing
+L3 = [21, 9.8, 'hi', 45, 765, 2001, 5, 2034, 'Hey']
+
+# WAP to retrieve the value 4 from the following nested list using positive indexing
+L4 = [21, ['hi', 'hello', [3, 4, 5]], 45, 765, 2001, 5, 2034, 'Jai']
+
+# WAP to retrieve the character 'u' from the following nested list using negative indexing
+L5 = [21, ['Anil', 'Education', ['Java', 'Kova']], 7065, 2001, 5, 2034, [1, 2]]
+
+# WAP to retrieve the 'Sweet' string from the following nested list using Positive indexing
+L6 = [21, ['Anil', 'Education', [['Java', 'Kova'], ['Programming', 'Sweet']]], 7065, 5, 2034, [1, 2]]
