259e10412e4d1ca861deeccdef1da555dc7a68d7
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-01 23:00:20 +0530

    Add files via upload

---
+++ class_programs/control_statements/conditional/milk_order_if.py
@@ -0,0 +1,5 @@
+# WAP to order a milk based on the milk availability
+milk_availability = input("Enter 'Yes' if milk available 'No' otherwise: ")
+if milk_availability == 'No':
+    print("Order the milk")
+print("Continue with normal activities")
---
+++ class_programs/control_statements/conditional/person-height-if-else.py
@@ -0,0 +1,1 @@
+# WAP to check the tall person based on person height
