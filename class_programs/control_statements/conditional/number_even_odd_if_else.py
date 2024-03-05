553f1a21b45cdbcfa0e2047b9f7607352205cc25
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-05 22:13:31 +0530

    Add files via upload

---
+++ class_programs/control_statements/conditional/number_even_odd_if_else.py
@@ -0,0 +1,7 @@
+# WAP to find given number is even or odd
+number = int(input("Enter a positive number to find even or odd: "))
+if number % 2 == 0:
+    print(number, "is a even number")
+else:
+    print(number, "is a odd number")
+print("End of the program")
