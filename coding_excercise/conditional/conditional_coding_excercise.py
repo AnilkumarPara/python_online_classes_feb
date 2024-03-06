ce5a2d3c6cb16b3bd431640b7bafe2a049fdc261
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-06 19:50:47 +0530

    Add files via upload

---
+++ coding_excercise/conditional/conditional_coding_excercise.py
@@ -0,0 +1,29 @@
+"""
+Coding exercise-1
+Write a program that calculates the shipping cost for an online order based on the
+total order value. Apply a 10% shipping fee if the order value is below $50;
+otherwise, offer free shipping.
+"""
+
+"""
+Coding exercise-2
+Write a program that takes a username and password as input,
+checks if the user's credentials match a predefined username and password.
+If they match, display a success message; otherwise, display a failure message.
+"""
+
+"""
+Coding exercise-3
+Write a program for an ATM machine that allows users to withdraw cash.
+The user should input the amount they want to withdraw, and the program
+should deduct the amount if the user has sufficient balance.
+Display an appropriate message if the withdrawal is successful
+or if there are insufficient funds.
+"""
+
+"""
+Coding exercise-4
+Write a program that takes the current hour as input (in 24-hour format) and
+displays a suitable greeting based on the time of the day
+(morning, afternoon, evening).
+"""
