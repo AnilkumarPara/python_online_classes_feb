5decceaf19182eab9fd5ad4c9c50bc1205b0a457
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-08 10:19:11 +0530

    Add files via upload

---
+++ class_programs/control_statements/loops/2_sum_numbers_list_for.py
@@ -0,0 +1,7 @@
+# WAP to find the sum of the numbers
+numbers = [1, 2, 3, 4, 5]
+
+total_sum = 0
+for number in numbers:
+    total_sum = total_sum + number
+print('sum of the values=', total_sum)
---
+++ class_programs/control_statements/loops/max-in-list-for.py
@@ -0,0 +1,3 @@
+# Find the maximum number in a list
+numbers = [1, 2, 3, 4, 5]
+
