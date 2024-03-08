1f124b5455c5ecf2abd8b9ff2143269faaa55d7a
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-08 10:20:16 +0530

    Add files via upload

---
+++ class_programs/control_statements/loops/1_fruits_for_loop.py
@@ -0,0 +1,5 @@
+# WAP to access each fruit from the fruits container
+fruits = ["Kevi", "banana", "orange", "apple"]
+for fruit in fruits:
+    print(fruit)
+print("End of the program")
---
+++ class_programs/control_statements/loops/print_numbers_1_5_while.py
@@ -0,0 +1,7 @@
+# WAP to print numbers from 1 to 5 on the screen
+i = 1
+n = int(input("Enter n value to print: "))
+while i <= n:
+    print(i)
+    i = i + 1
+print("End of the program")
