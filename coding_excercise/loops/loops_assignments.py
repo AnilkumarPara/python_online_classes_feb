764f0ce355df902c8478748f4da1a0cd96a5f801
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-12 23:08:49 +0530

    Update loops_assignments.py

--- coding_excercise/loops/loops_assignments.py
+++ coding_excercise/loops/loops_assignments.py
@@ -16,3 +16,11 @@ n = int(input("Enter the number to calculate the factorial "))
 A palindrome number is a number that remains the same when its digits are reversed.
 For example, 121, 1331, 16461 are palindrome numbers, while 123 and 321 are not.
 """
+
+# WAP to unlock the mobile device using a pattern
+"""
+User enters a correct password in 5 attempts, phone will be unlocked.
+If a user enters an incorrect password for 5 attempts,
+You have to wait for 30 seconds to try again.
+This process repeats
+"""
