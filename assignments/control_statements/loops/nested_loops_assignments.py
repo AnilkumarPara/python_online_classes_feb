7e6c0992258b1dbf390dbe628e7d943925ff6b25
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-15 13:31:37 +0530

    Add files via upload

---
+++ assignments/control_statements/loops/nested_loops_assignments.py
@@ -0,0 +1,14 @@
+# Find all prime numbers between 2 and n
+"""
+The standard definition of prime numbers applies only to
+positive integers greater than 1. A prime number is defined
+as a number that has only two distinct positive divisors: 1 and itself.
+For example, 2, 3, 5, 7, 11, etc. are prime numbers
+because they can only be divided evenly by 1 and themselves
+without leaving a remainder.
+"""
+
+# Print multiplication tables from 1 to n
+"""
+Let's say if user inputs 9, you need to generate 1 to 9 tables
+"""
