bf35141f3ff9056da8495904ab3b1692e413de49
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-19 21:58:38 +0530

    Add files via upload

---
+++ assignments/functions/function_assignments.py
@@ -0,0 +1,9 @@
+"""
+1. Array and String Manipulation
+Question: Given an array of integers, write a function that
+returns the array with all zeros moved to the end while
+maintaining the relative order of the non-zero elements.
+input: [2, 0, 1, 34, 0, 900]
+output: [2, 1, 34, 900, 0, 0]
+"""
+
