d94bffae0b6d3af0e43f3d4f54dcb08e9207faac
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-28 10:18:43 +0530

    Add files via upload

--- class_programs/comprehensions/square_numbers_dict_comp.py
+++ class_programs/comprehensions/square_numbers_dict_comp.py
@@ -3,7 +3,4 @@ Creating a dictionary of squares for numbers from 1 to 5
 using dictionary comprehension
 """
 numbers = [1, 2, 3, 4, 5]
-squared_numbers = {}
-for number in numbers:
-    squared_numbers.setdefault(number, number*number)
-print(squared_numbers)
+print({number:number * number for number in numbers})
