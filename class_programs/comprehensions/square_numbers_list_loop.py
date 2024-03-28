5b0308222a5d50067c95c5306625a64f14190769
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-28 10:20:59 +0530

    Add files via upload

---
+++ class_programs/comprehensions/square_numbers_list_loop.py
@@ -0,0 +1,6 @@
+# WAP to square each number in the list using for loop
+numbers = [1, 2, 3, 4, 5]
+squared_list = []
+for number in numbers:
+    squared_list.append(number * number)
+print(squared_list)
