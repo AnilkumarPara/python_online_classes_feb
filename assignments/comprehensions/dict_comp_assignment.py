bc24f751d7d6aff7e9dea7ccbcda4ba591914cc4
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-03-28 10:30:54 +0530

    Add files via upload

---
+++ assignments/comprehensions/dict_comp_assignment.py
@@ -0,0 +1,27 @@
+"""
+Assignment-1:
+Count the frequency of letters in a string and output as
+key: value
+char:count
+Write program using dictionary comprehension
+"""
+string = "hello"
+
+
+"""
+Assignment-2:
+create dictionary with string of lengths and output as
+key: value
+word:length
+Write program using dictionary comprehension
+"""
+words = ["hey", "Hello", "Covid"]
+
+"""
+Assignment-3:
+Count the frequency of each fruit in a list and output as
+key: value
+fruit:count
+Write program using dictionary comprehension
+"""
+fruits = ["apple", "banana", "apple", "cherry", "banana"]
---
+++ assignments/comprehensions/list_comp_assignment.py
@@ -0,0 +1,17 @@
+"""
+Assignment-1:
+WAP to find names starts with ‘A’ and ends with 'a'
+in the given list of names using list comprehension
+"""
+names = ['Abeesha', 'Abelia', 'Anil', 'Shanvika', 'Jayansh']
+
+"""
+Assignment-2:
+WAP to extract each word from a sentence and store all the words
+in the form of list using list comprehension
+"""
+
+paragraph = ["There was a fox.", 'It was brown in color.', "It was seen near that farm sometime back"]
+
+
+
