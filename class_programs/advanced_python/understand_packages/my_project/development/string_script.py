ecc9b19a350a8faf160fb1e004291b4609790538
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-04-23 20:50:01 +0530

    Add files via upload

---
+++ class_programs/advanced_python/understand_packages/my_project/development/string_script.py
@@ -0,0 +1,8 @@
+# Find Vowels
+# from my_project.development.strings import find_vowels_in_string
+# find_vowels_in_string.count_display_vowels('Anil')
+
+# import sys
+# print(sys.path)
+from strings import find_vowels_in_string
+find_vowels_in_string.count_display_vowels('Sunil')
---
+++ class_programs/advanced_python/understand_packages/my_project/python_scripts/pattern_script.py
@@ -0,0 +1,3 @@
+# Diamond pattern
+from my_project.development.patterns import pattern_diamond
+pattern_diamond.diamond_pattern(3)
